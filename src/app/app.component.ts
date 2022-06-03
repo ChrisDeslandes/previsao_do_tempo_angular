27a1a2afaff80c9ed1b57d5cb7d9d9f92a4e9c93
Christiano Deslandes Ribeiro <petracconi@gmail.com>
2022-06-03 18:33:41 -0300

    Cópia do outro app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'api_previsao_do_tempo';
+  title = 'Previsão do Tempo';
 }
